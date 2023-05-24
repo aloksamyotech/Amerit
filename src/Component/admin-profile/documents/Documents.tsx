c3a084cf7c405cc300598536edeb2da7a1eee330
harshit22 <harshitmehta155@gmail.com>
2023-05-24 18:41:55 +0530

    tr

--- src/Component/admin-profile/documents/Documents.tsx
+++ src/Component/admin-profile/documents/Documents.tsx
@@ -39,6 +39,7 @@ const Documents = () => {
     file6: [],
   })
   const hiddenFileInput = React.useRef<HTMLInputElement>(null);
+  const hiddenFileInput2 = React.useRef<HTMLInputElement>(null);
   const [progress, setProgress] = React.useState(10);
   const [documentTypes, setDocumentTypes] = useState();
   const [show, setShow] = useState(false);
@@ -66,8 +67,12 @@ const Documents = () => {
   };
   const [showComponent, setShowComponent] = useState(false);
 
-  const handleClick = () => {
+  const handleClick1 = () => {
     hiddenFileInput?.current?.click();
+    setShowComponent(true);
+  }
+  const handleClick = () => {
+    hiddenFileInput2?.current?.click();
     setShowComponent(true);
   }
   // const handleClose = () => {
@@ -133,7 +138,7 @@ const Documents = () => {
                           borderColor: (theme: Theme) => theme.palette.secondary.main
                         }}
                           style={CBox}
-                          onClick={() => handleClick()}
+                          onClick={() => handleClick1()}
                         >
                           <Grid container lg={12}>
                             <Grid xs={12}>
@@ -211,11 +216,107 @@ const Documents = () => {
 
                   </Grid>
                 </Grid>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Typography variant='h6'>ACH Form</Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box
+                      width={'100%'}
+                      display='flex'
+                      alignItems='center'
+                      justifyContent='center'
+                      textAlign='center'
+                    >
 
-              {/* </> */}
-            {/* ) */}
-          {/* })} */}
-          {/* <FileUploadPage /> */}
+
+                      {selectedFiles.file2.length == 0 && (
+                        <Grid sx={{
+                          cursor: 'pointer',
+                          width: '100%',
+                          color: (theme: Theme) => theme.palette.linkBlue.main,
+                          border: '2px dotted',
+                          borderColor: (theme: Theme) => theme.palette.secondary.main
+                        }}
+                          style={CBox}
+                          onClick={() => handleClick()}
+                        >
+                          <Grid container lg={12}>
+                            <Grid xs={12}>
+                              <FileUploadOutlined />
+                            </Grid>
+                            <Grid xs={12}>
+                              <input
+                                type='file'
+                                ref={hiddenFileInput2}
+                                style={Input}
+                                onChange={(event) => handleFileChange(event, 'file2')}
+                              />
+                              {selectedFiles.file2.length > 0 && (
+                                <>
+                                  {show ? <Uploading /> : null}
+                                </>
+                              )}
+                              <Grid sx={{ color: '#000', mr: '5px', display: 'inline-block' }}>Drag & Drop or</Grid>
+                              <Button onClick={() => handleUpload('file1')}>Choose File</Button><Grid sx={{ color: '#000', ml: '5px', display: 'inline-block' }}> to Upload</Grid>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                     )}
+
+                      {selectedFiles.file2.length > 0 && (
+                        <>
+                          {show ? <Uploading /> : (
+                            <>
+                            {selectedFiles.file2 && (
+                              <>
+                                <Grid item xs={12} >
+                                  <Box
+                                    width={'100%'}
+                                    alignItems='center'
+                                    sx={{
+                                      p: 1,
+                                      border: '1px solid ',
+                                      borderColor: (theme: Theme) => theme.palette.primary.main,
+                                      marginTop: '20px'
+                                    }}
+                                  >
+                                    <Box display='flex' width='100%' sx={{ p: 1 }}>
+                                      <Image
+                                        src='/images/pdf.svg'
+                                        height={38}
+                                        width={34}
+                                        alt='Follow us on Twitter'
+                                      />
+                                      <Box
+                                        display='flex'
+                                        justifyContent='space-between'
+                                        width='100%'
+                                      >
+                                        <Box sx={{ ml: 2 }}>
+                                          <Typography variant='subtitle2'>
+                                            {selectedFiles.file2[0].name}
+                                          </Typography>
+                                          <Typography variant='caption'>443 kb</Typography>
+                                        </Box>
+                                        <IconButton onClick={() => handleRemoveFile('file1')}>
+                                          <Close />
+                                        </IconButton>
+                                      </Box>
+                                    </Box>
+                                  </Box>
+                                </Grid>
+                              </>
+                            )}
+                            </>
+                          )}
+                        </>
+                      )}
+
+                    </Box>
+
+                  </Grid>
+                </Grid>
           <Grid container item spacing={2}>
             <Grid item xs={6}>
               <Box width={'100%'} pt={2}>
